020e8c88a61c08837d1cf76431e411901bb83766
benhunt29 <benhunt29@gmail.com>
2015-09-24 16:16:59 -0500

    started questionnaire logic

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -104,9 +104,6 @@ app.controller('helpController',['$scope', function($scope){
 
 app.controller('questionnaireController',['$rootScope','$scope','$location', '$http', function($rootScope,$scope,$location,$http){
 
-    var questions = [{question: "Are you interested in living abroad?",type:'boolean'},
-        {question:"Do you speak any languages besides English?",type:'boolean'},
-    {question:"What field do you work in?",type:'list',answerOptions:['Technology','Engineering','Finance','Law','Business']}];
     var questionNum = 1;
     $scope.type = questions[0].type;
     $scope.question = questions[0].question;
@@ -211,5 +208,87 @@ app.factory('authInterceptor', ['$q', '$location', 'authService', function ($q, 
             }
             return $q.reject(response);
         }
+    };
+}]);
+
+app.factory('questionnaire', ['$q', '$location', 'authService', function ($q, $location, authService) {
+    return {
+        questions: [
+            {
+                question: "What language(s) do you speak?", type: 'list',
+                answerOptions: ['Adyghe', 'Albanian', 'Aragonese', 'Armenian', 'Aromanian', 'Arpitan', 'Asturian', 'Avar', 'Azerbaijani', 'Bashkir', 'Basque', 'Belarusian', 'Bosnian', 'Breton', 'Bulgarian', 'Catalan', 'Chechen', 'Chuvash', 'Cornish', 'Corsican', 'Crimean', 'Tatar', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'Erzya', 'Estonian', 'Faroese', 'Finnish', 'French', 'Frisian', 'Gagauz', 'Galician', 'Gallo', 'Georgian', 'German', 'Greek', 'Hungarian', 'Icelandic', 'Ingrian', 'Irish', 'Italian', 'Kabardian', 'Kashubian', 'Kazakh', 'Ladin', 'Latin', 'Latvian', 'Laz', 'Lithuanian', 'Luxembourgish', 'Macedonian', 'Maltese', 'Manx', 'Mari', 'Mingrelian', 'Mirandese', 'Montenegrin', 'Norwegian', 'Occitan', 'Ossetian', 'Picard', 'Polish', 'Portuguese', 'Romani', 'Romanian', 'Romansh', 'Russian', 'Sami', 'Sardinian', 'Scots', 'Scottish' 'Gaelic', 'Serbian', 'Silesian', 'Slovak', 'Slovene', 'Sorbian', 'Spanish', 'Svan', 'Swedish', 'Tabasaran', 'Tatar', 'Turkish', 'Ukrainian', 'Vepsian', 'Võro', 'Walloon', 'Welsh', 'Wymysorys']
+            },
+            {
+                question: 'What field do you work in?',
+                type: 'list',
+                answerOptions: ['Agriculture', 'Industry', 'Services']
+            },
+            {
+                question: 'What climate would you prefer?',
+                type: 'list',
+                answerOptions: ['Temperate', 'Mediterranean', 'Tropical', 'Arid', 'Desert', 'Maritime', 'Wet']
+            },
+            {
+                question: 'What is the ideal size for the largest metropolitan area?',
+                type: 'list',
+                answerOptions: ['Small (<100,000', 'Medium (100,000-1 million', 'Large (>1 million)']
+            },
+            {
+                question: 'What is your ideal size for the largest metropolitan area?',
+                type: 'list',
+                answerOptions: ['Small (<100,000', 'Medium (100,000-1 million', 'Large (>1 million)']
+            },
+            {
+                question: 'Would you like the population distribution to be mostly urban?',
+                type: 'boolean'
+            },
+            {
+                question: 'What would your ideal median age range be?', type: 'list',
+                answerOptions: ['20-30yrs', '30-40yrs', '40+years']
+            },
+            {
+                question: 'What cost of living should your destination have?', type:'list',
+                answerOptions: ['High','Average','Low']
+            },
+            {
+                question: 'What percentage of the population should be internet users?', type:'list',
+                answerOptions: ['<25%','25%-50%','50%-75%','>75%']
+            }
+        ],
+        determineRecommendations: function(questionnaireAnswers,countriesToSearch){
+
+            countries = [];
+            countriesToSearch.forEach(function(country,index) {
+                var score = 0;
+                labor = labor;
+                climate = climate;
+                perCapitaPPP;
+                urbanPopulation;
+                largestCityPop;
+                medianAge;
+                internetUsagePerCapita;
+                country.score = score;
+                countries.push(country);
+            });
+
+            countries.sort(function (a, b) {
+                    if (a.score > b.score) {
+                        return -1;
+                    }
+                    if (a.score < b.score) {
+                        return 1;
+                    }
+                    if(a.countryName > b.countryName){
+                        return 1;
+                    }
+                    if(a.countryName < b.countryName){
+                        return -1;
+                    }
+                    return 0;
+                });
+
+                var recommendedCountries = countries.slice(0,4);
+        }
+
     };
 }]);
